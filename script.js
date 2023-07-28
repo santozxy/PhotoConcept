046763c2de953d39be07e216da46023c3d58f433
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2023-07-28 13:19:56 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+AOS.init();
+
+function openTab(evt, nav) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(nav).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("essays").style.display = "block";
+});
